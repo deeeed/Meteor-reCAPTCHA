b690a70ef16a1dbb4252dd757048ec41f1feb9aa
deeeed <abreton@siteed.net>
2015-11-29 19:44:33 +0800

    fix version

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
     name: "deeeed:recaptcha",
     summary: "(timeout + configurable callbacks) Implementation of Google reCAPTCHA V2 for Meteor",
     git: "https://github.com/deeeed/Meteor-reCAPTCHA.git",
-    version: "2.0.6",
+    version: "2.0.7",
     license: "MIT"
 });
 
 Package.onUse(function(api) {
 
-    api.versionsFrom('1.1.0');
+    api.versionsFrom('1.0.2');
 
     api.use([
         'templating',
